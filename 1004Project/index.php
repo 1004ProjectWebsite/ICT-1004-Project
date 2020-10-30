2ac8af25192635926576d5c8351c54ecbab54aac
KahWei <kahweing96@gmail.com>
2020-10-31 02:57:54 +0800

    Layout skeleton

---
+++ 1004Project/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+    <title>Phone Case Shop</title>
+   <?php
+    include "head.inc.php";
+    ?>
+    </head>
+    <body> 
+         <?php
+            include "nav.inc.php";
+         ?>
+        <header class="jumbotron text-center">        
+            <h1 class="display-4">Welcome to ______</h1>
+            <p>at introduces the company/organization</p>
+        </header>
+
+
+        <main class="container">
+       
+        </main>
+
+    </body>
+</html>
---
+++ 1004Project/nav.inc.php
@@ -0,0 +1,39 @@
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">LAB-P7 - 3</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="product.php">Shop
+               <i class="material-icons" style="font-size:1em">store</i>
+           </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="aboutus.php">About Us</a>
+            </li>
+            </ul>
+        
+            <ui class="navbar-nav ml-auto">
+                 <li class="nav-item">
+                <a class="nav-link" title="Shopping Cart" href="#">
+                    <i class="material-icons" style="font-size:2em">shopping_cart</i>
+                </a>
+            </li> 
+                <li class="nav-item">
+                <a class="nav-link" title="Create Account" href="register.php">
+                    <i class="material-icons" style="font-size:2em">account_circle</i>
+                </a>
+            </li> 
+            <li class="nav-item">
+                <a class="nav-link" title="Sign In" href="login.php">
+                    <i class="material-icons" style="font-size:2em">login</i>
+                </a>
+            </li> 
+        </ul>
+    </div>  
+</nav>
