e573e8b3912fe2dc6953c64c9c63929fbc0d89b2
guancong <guancong96@gmail.com>
2020-11-06 19:46:45 +0800

    Added parallax scrolling, and title. Created main.css added bg image

--- 1004_Project/index.php
+++ 1004_Project/index.php
@@ -6,23 +6,28 @@ and open the template in the editor.
 -->
 <html>
     <head>
-    <title>Phone Case Shop</title>
-   <?php
-    include "head.inc.php";
-    ?>
+        <title>Phone Case Shop</title>
+        <?php
+        include "head.inc.php";
+        ?>
     </head>
     <body> 
-         <?php
-            include "nav.inc.php";
-         ?>
-        <header class="jumbotron text-center">        
+        <?php
+        include "nav.inc.php";
+        ?>
+        <header class="jumbotron parallax text-center"> 
+            <div class="title">
             <h1 class="display-4">Welcome to ______</h1>
             <p>at introduces the company/organization</p>
+            </div>
         </header>
 
 
         <main class="container">
-       
+            <p>Scroll Up and Down.</p>
+            <div style="height:1000px;background-color:white;font-size:36px">
+                test
+            </div>
         </main>
 
     </body>
