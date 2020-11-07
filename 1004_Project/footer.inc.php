85f230aabcc824637f4395bf4120db65aeb2a2a4
guancong <guancong96@gmail.com>
2020-11-07 20:51:20 +0800

    1. Added footer and fixed other bugs

--- 1004_Project/footer.inc.php
+++ 1004_Project/footer.inc.php
@@ -1,4 +1,40 @@
-<footer class="container">
-    <p>Copyright &copy; 2020 Phone case Pte.Ltd.</p>
+<footer class="footer z-2">
+    <!-- FOOTER START -->
+        <div class="contain" style="align-content: center;">
+            <div class="row">
+            <div class="col">
+                <h1>Company</h1>
+                <ul>
+                    <li>About</li>                
+                    <li>Social</li>
+                    <li>Get in touch</li>
+                </ul>
+            </div>
+            <div class="col">
+                <h1>Products</h1>
+                <ul>
+                    <li>About</li>
+                    <li>Social</li>
+                    <li>Get in touch</li>
+                </ul>
+            </div>           
+            <div class="col">
+                <h1>Support</h1>
+                <ul>
+                    <li>Contact us</li>
+                </ul>
+            </div>
+            <div class="col social">
+                <h1>Social</h1>
+                <ul>
+                    <li><img src="images/email-footer.png"  width="32" style="width: 32px;"></li>            
+                    <li><img src="images/instagram-footer.png" width="32" style="width: 32px;"></li>
+                    <li><img src="images/facebook-footer.png" width="32" style="width: 32px;"></li>
+                </ul>
+            </div>
+            </div>
+            <div class="clearfix"> </div>
+        </div>  
+    <!-- END OF FOOTER -->
 </footer>
 
--- 1004_Project/index.php
+++ 1004_Project/index.php
@@ -1,110 +1,97 @@
 <?php
 session_start(); //start session
 ?>
+<head>
+    <title>Phone Case Shop</title>
+    <?php
+    include "head.inc.php";
+    ?>
+</head>
 
-<html>
-    <head>
-        <meta http-equiv="Content-Type" conte...
-              <!DOCTYPE html>
-        <!--
-        To change this license header, choose License Headers in Project Properties.
-        To change this template file, choose Tools | Templates
-        and open the template in the editor.
-        -->
-    <html>
-        <head>
-            <title>Phone Case Shop</title>
-            <?php
-            include "head.inc.php";
-            ?>
-        </head>
-        <body>
-            <?php
-            include "nav.inc.php";
-            ?>
-            <header class="jumbotron parallax text-center">
-                <div class="title">
-                    <h1 class="display-4">Welcome to ______</h1>
-                    <p>at introduces the company/organization</p>
-                </div>
-            </header>
+<body>
+    <?php
+    include "nav.inc.php";
+    ?>
+    <header class="jumbotron parallax text-center" style="margin: 20px;">
+        <div>
+            <h1 class="display-4">Welcome to ______</h1>
+            <p>at introduces the company/organization</p>
+        </div>
+    </header>
 
-            <main class="container">
-                <p>Scroll Up and Down.</p>
-                <div style="height:1000px;background-color:white;font-size:36px">
-                    <h3>Popular items</h3>
-                    
-                   <!--put image in 3 column -->
-                    <div class="container">
-                        <div class="row">
-                            <div class="col">
-                                <article>
-                                    <figure>
-                                        <a href="phone_cases_img/iphone_11_clearcase.jpg">
-                                            <img src="phone_cases_img/iphone_11_clearcase.jpg" alt="iphone11"/>
-                                        </a>
-                                        <figcaption>iPhone 11 clear case</figcaption>
-                                    </figure>
-                                </article>
-                            </div>
-                            <div class="col">
-                                <article >
-                                    <figure>
-                                        <a href="phone_cases_img/iphone_12_kumquat.jpg">
-                                            <img src="phone_cases_img/iphone_12_kumquat.jpg" alt="iphone12"/>
-                                        </a>
-                                        <figcaption>iPhone 12 Silicon case - Kumquat</figcaption>
-                                    </figure>
-                                </article>
-                            </div>
-                            <div class="col">
-                                <article >
-                                    <figure>
-                                        <a href="phone_cases_img/iphone_12_Green.jpg">
-                                            <img src="phone_cases_img/iphone_12_Green.jpg" alt="iphone12"/>
-                                        </a>
-                                        <figcaption>iPhone 12 Silicon case - Green</figcaption>
-                                    </figure>
-                                </article>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col">
-                                <article>
-                                    <figure>
-                                        <a href="phone_cases_img/samsung_note20_clear.jpg">
-                                            <img src="phone_cases_img/samsung_note20_clear.jpg" alt="Samsungnote20"/>
-                                        </a>
-                                        <figcaption>Samsung note20 clear</figcaption>
-                                    </figure>
-                                </article>
-                            </div>
-                            <div class="col">
-                                <article>
-                                    <figure>
-                                        <a href="phone_cases_img/huawei_p30_red.jpg">
-                                            <img src="phone_cases_img/huawei_p30_red.jpg" alt="huaweip30"/>
-                                        </a>
-                                        <figcaption>HuaWei P30 silicon case red</figcaption>
-                                    </figure>
-                                </article>
-                            </div>
-                            <div class="col">
-                                <article>
-                                    <figure>
-                                        <a href="phone_cases_img/samsung_note20_brown.jpg">
-                                            <img src="phone_cases_img/samsung_note20_brown.jpg" alt="Samsungnote20"/>
-                                        </a>
-                                        <figcaption>Samsung note20 Leather cover - brown</figcaption>
-                                    </figure>
-                                </article>
-                            </div>
-                        </div>
-
+    <main class="container">
+            <h3>Popular items</h3>
+            <!--put image in 3 column -->
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <article>
+                            <figure>
+                                <a href="phone_cases_img/iphone_11_clearcase.jpg">
+                                    <img src="phone_cases_img/iphone_11_clearcase.jpg" alt="iphone11"/>
+                                </a>
+                                <figcaption>iPhone 11 clear case</figcaption>
+                            </figure>
+                        </article>
                     </div>
-            </main>
-            <?php
-            include "footer.inc.php";
-            ?>
-        </body>
-    </html>
+                    <div class="col">
+                        <article >
+                            <figure>
+                                <a href="phone_cases_img/iphone_12_kumquat.jpg">
+                                    <img src="phone_cases_img/iphone_12_kumquat.jpg" alt="iphone12"/>
+                                </a>
+                                <figcaption>iPhone 12 Silicon case - Kumquat</figcaption>
+                            </figure>
+                        </article>
+                    </div>
+                    <div class="col">
+                        <article >
+                            <figure>
+                                <a href="phone_cases_img/iphone_12_Green.jpg">
+                                    <img src="phone_cases_img/iphone_12_Green.jpg" alt="iphone12"/>
+                                </a>
+                                <figcaption>iPhone 12 Silicon case - Green</figcaption>
+                            </figure>
+                        </article>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <article>
+                            <figure>
+                                <a href="phone_cases_img/samsung_note20_clear.jpg">
+                                    <img src="phone_cases_img/samsung_note20_clear.jpg" alt="Samsungnote20"/>
+                                </a>
+                                <figcaption>Samsung note20 clear</figcaption>
+                            </figure>
+                        </article>
+                    </div>
+                    <div class="col">
+                        <article>
+                            <figure>
+                                <a href="phone_cases_img/huawei_p30_red.jpg">
+                                    <img src="phone_cases_img/huawei_p30_red.jpg" alt="huaweip30"/>
+                                </a>
+                                <figcaption>HuaWei P30 silicon case red</figcaption>
+                            </figure>
+                        </article>
+                    </div>
+                    <div class="col">
+                        <article>
+                            <figure>
+                                <a href="phone_cases_img/samsung_note20_brown.jpg">
+                                    <img src="phone_cases_img/samsung_note20_brown.jpg" alt="Samsungnote20"/>
+                                </a>
+                                <figcaption>Samsung note20 Leather cover - brown</figcaption>
+                            </figure>
+                        </article>
+                    </div>
+                </div>
+
+            </div>
+    </main>        
+</body>
+
+<?php
+include "footer.inc.php";
+?>
