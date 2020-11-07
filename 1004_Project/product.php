5e58a1bf27a81fa245a6ca56003a73ac5c92683f
KahWei <kahweing96@gmail.com>
2020-11-07 12:46:16 +0800

    Shop page pulling from database

--- 1004_Project/product.php
+++ 1004_Project/product.php
@@ -1,3 +1,8 @@
+<?php
+//session_start();
+$con = mysqli_connect("localhost", "root", "kahwei", "1004_project");
+?>
+<!doctype html>
 <html>
     <head>
         <title>Phone Case Shop</title>
@@ -5,14 +10,39 @@
         include "head.inc.php";
         ?>
     </head>
-    <body> 
+    <body>
         <?php
         include "nav.inc.php";
         ?>
-        <-<!-- testing for scrolling -->
-        <p>Scroll Up and Down.</p>
-        <div style="height:1000px;background-color:white;font-size:36px"></div>
-        
-        <div class="products">
-
-        </div>
+        <!-- testing for scrolling -->
+        <p>Scroll Up and Down.</p> 
+        <?php
+        $query = $con->prepare("SELECT * FROM products");
+        $query->execute();
+        $result = $query->get_result();
+        while ($row = $result->fetch_array()) {
+            ?>
+            <div class="container">
+                <div class="col-sm-3">                
+                    <?php
+                    echo '<img src="data:image/jpeg;base64,' . base64_encode($row['p_img']) . '"/>';
+                    ?>    
+                    <h5 class="text-body"><?php echo $row["p_name"]; ?></h5>
+                    <h5 class="text-info"><?php echo "$", $row["p_price"]; ?></h5>
+                    <input type="text" name="quantity" class="form-control" value="1">
+                    <input type="hidden" name="hidden_name" value="<?php echo $row["p_name"]; ?>">
+                    <input type="hidden" name="hidden_price" value="<?php echo $row["p_price"]; ?>">
+                    <input type="submit" name="add" style="margin-top: 5px;" class="btn btn-success" value="Add to Cart">
+                </div>
+            </div>
+            <?php
+        }
+        $query->close();
+        $con->close();
+        ?>
+    </div>
+</body>
+<?php
+include "footer.inc.php";
+?>
+</html>
