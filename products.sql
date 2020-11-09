46dc134219868bd9fc6d57dd7f270c857422cf6c
Nick <nbinged@gmail.com>
2020-11-10 01:10:46 +0800

    Added seed data for database
    Run products.sql to add in seed data to db.
    
    Added store.php to work on store layout

---
+++ products.sql
@@ -0,0 +1,30 @@
+INSERT INTO products (product_id, p_name, p_price, p_img, p_desc)
+VALUES ('1', 'Huawei P30 Silicone Case', '21', 'huawei_p30_red.jpg'
+, "This official Huawei Silicone case for the Huawei P30 in Red offers excellent protection while maintaining your device's sleek, lines. 
+As an official product, it is designed specifically for the P30 and allows full access to buttons and ports.");
+
+INSERT INTO products (product_id, p_name, p_price, p_img, p_desc)
+VALUES ('2', 'Iphone 11 Clear Case', '30', 'iphone_11_clearcase.JPG'
+, "Introducing the New Case range from Apple. Thin, light and easy to grip — 
+this Apple designed clear case shows off the brilliant coloured, while providing extra protection. 
+This case protects your iPhone 11 Pro Max from scratches, scuffs & bumps.");
+
+INSERT INTO products (product_id, p_name, p_price, p_img, p_desc)
+VALUES ('3', 'Iphone 12 Silicone Case (Green)', '35', 'iphone_12_Green.JPG'
+, "Designed by Apple to complement iPhone 12 | 12 Pro, the Silicone Case with MagSafe is a delightful way to protect your iPhone.
+The silky, soft-touch finish of the silicone exterior feels great in your hand. And on the inside, there’s a soft microfibre lining for even more protection.");
+
+INSERT INTO products (product_id, p_name, p_price, p_img, p_desc)
+VALUES ('4', 'Iphone 12 Silicone Case (Kumquat)', '30', 'iphone_12_kumquat.JPG'
+, "Designed by Apple to complement iPhone 12 | 12 Pro, the Silicone Case with MagSafe is a delightful way to protect your iPhone.
+The silky, soft-touch finish of the silicone exterior feels great in your hand. And on the inside, there’s a soft microfibre lining for even more protection.");
+
+INSERT INTO products (product_id, p_name, p_price, p_img, p_desc)
+VALUES ('5', 'Samsung Note20 Leather Case (Brown)', '18', 'samsung_note20_brown.jpg'
+, "This Official Samsung Leather Cover in Brown is the perfect way to keep your 
+Samsung Galaxy Note 20 Ultra smartphone protected in style, 
+made out of genuine leather. Compatible with 4G and 5G variants.");
+
+INSERT INTO products (product_id, p_name, p_price, p_img, p_desc)
+VALUES ('6', 'Samsung Note20 Leather Case (Clear)', '18', 'samsung_note20_clear.jpg'
+, "This Official Samsung Clear Cover is the perfect accessory for your Samsung Galaxy Note 20 smartphone.");
