d2bf3b67fd97777ae0778896c483fe6167fa2fe1
juanjuan7699 <71871190+juanjuan7699@users.noreply.github.com>
2020-11-11 15:29:07 +0800

    add name logo

--- 1004_Project/index.php
+++ 1004_Project/index.php
@@ -6,6 +6,19 @@ session_start(); //start session
     <?php
     include "head.inc.php";
     ?>
+    <style>
+        article {
+            text-align: center;
+        }
+        
+        #companyInfo {
+            margin-top: -50px;
+        }
+        
+        #logo {
+            height: 170px;
+        }
+    </style>
 </head>
 
 <body>
@@ -14,112 +27,112 @@ session_start(); //start session
     ?>
     <header class="jumbotron parallax text-center" style="margin: 20px;">
         <div>
-            <h1 class="display-4">Welcome to ______</h1>
-            <p>at introduces the company/organization</p>
+            <h1 class="display-4">Welcome to<img id="logo" src="images/sphone_logo_transparent.png"></h1>
+            <p id="companyInfo">at introduces the company/organization</p>
         </div>
     </header>
 
     <main class="container">
-            <h3>Popular items</h3>
-            <!--put image in 3 column -->
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <article>
-                            <figure>
-                                <a href="phone_cases_img/iphone_11_clearcase.jpg">
-                                    <img src="phone_cases_img/iphone_11_clearcase.jpg" alt="iphone11"/>
-                                </a>
-                                <figcaption>iPhone 11 clear case</figcaption>
-                            </figure>
-                        </article>
-                    </div>
-                    <div class="col">
-                        <article >
-                            <figure>
-                                <a href="phone_cases_img/iphone_12_kumquat.jpg">
-                                    <img src="phone_cases_img/iphone_12_kumquat.jpg" alt="iphone12"/>
-                                </a>
-                                <figcaption>iPhone 12 Silicon case - Kumquat</figcaption>
-                            </figure>
-                        </article>
-                    </div>
-                    <div class="col">
-                        <article >
-                            <figure>
-                                <a href="phone_cases_img/iphone_12_Green.jpg">
-                                    <img src="phone_cases_img/iphone_12_Green.jpg" alt="iphone12"/>
-                                </a>
-                                <figcaption>iPhone 12 Silicon case - Green</figcaption>
-                            </figure>
-                        </article>
-                    </div>
+        <h3>Popular items</h3>
+        <!--put image in 3 column -->
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <article>
+                        <figure>
+                            <a href="phone_cases_img/iphone_11_clearcase.jpg">
+                                <img src="phone_cases_img/iphone_11_clearcase.jpg" alt="iphone11"/>
+                            </a>
+                            <figcaption>iPhone 11 clear case</figcaption>
+                        </figure>
+                    </article>
                 </div>
-                <div class="row">
-                    <div class="col">
-                        <article>
-                            <figure>
-                                <a href="phone_cases_img/samsung_note20_clear.jpg">
-                                    <img src="phone_cases_img/samsung_note20_clear.jpg" alt="Samsungnote20"/>
-                                </a>
-                                <figcaption>Samsung note20 clear</figcaption>
-                            </figure>
-                        </article>
-                    </div>
-                    <div class="col">
-                        <article>
-                            <figure>
-                                <a href="phone_cases_img/huawei_p30_red.jpg">
-                                    <img src="phone_cases_img/huawei_p30_red.jpg" alt="huaweip30"/>
-                                </a>
-                                <figcaption>HuaWei P30 silicon case red</figcaption>
-                            </figure>
-                        </article>
-                    </div>
-                    <div class="col">
-                        <article>
-                            <figure>
-                                <a href="phone_cases_img/samsung_note20_brown.jpg">
-                                    <img src="phone_cases_img/samsung_note20_brown.jpg" alt="Samsungnote20"/>
-                                </a>
-                                <figcaption>Samsung note20 Leather cover - brown</figcaption>
-                            </figure>
-                        </article>
-                    </div>
+                <div class="col">
+                    <article >
+                        <figure>
+                            <a href="phone_cases_img/iphone_12_kumquat.jpg">
+                                <img src="phone_cases_img/iphone_12_kumquat.jpg" alt="iphone12"/>
+                            </a>
+                            <figcaption>iPhone 12 Silicon case - Kumquat</figcaption>
+                        </figure>
+                    </article>
                 </div>
-                <div class="row">
-                    <div class="col">
-                        <article>
-                            <figure>
-                                <a href="phone_cases_img/iphone_12_starrysky.jpg">
-                                    <img src="phone_cases_img/iphone_12_starrysky.jpg" alt="iPhone12"/>
-                                </a>
-                                <figcaption>iPhone 12 Starry Sky</figcaption>
-                            </figure>
-                        </article>
-                    </div>
-                    <div class="col">
-                        <article>
-                            <figure>
-                                <a href="phone_cases_img/iphone_x_dog.jpg">
-                                    <img src="phone_cases_img/iphone_x_dog.jpg" alt="iPhoneX"/>
-                                </a>
-                                <figcaption>iPhone X Dog</figcaption>
-                            </figure>
-                        </article>
-                    </div>
-                    <div class="col">
-                        <article>
-                            <figure>
-                                <a href="phone_cases_img/oppo_doraemon.jpg">
-                                    <img src="phone_cases_img/oppo_doraemon.jpg" alt="Oppo"/>
-                                </a>
-                                <figcaption>Oppo Doraemon</figcaption>
-                            </figure>
-                        </article>
-                    </div>
+                <div class="col">
+                    <article >
+                        <figure>
+                            <a href="phone_cases_img/iphone_12_Green.jpg">
+                                <img src="phone_cases_img/iphone_12_Green.jpg" alt="iphone12"/>
+                            </a>
+                            <figcaption>iPhone 12 Silicon case - Green</figcaption>
+                        </figure>
+                    </article>
                 </div>
             </div>
+            <div class="row">
+                <div class="col">
+                    <article>
+                        <figure>
+                            <a href="phone_cases_img/samsung_note20_clear.jpg">
+                                <img src="phone_cases_img/samsung_note20_clear.jpg" alt="Samsungnote20"/>
+                            </a>
+                            <figcaption>Samsung note20 clear</figcaption>
+                        </figure>
+                    </article>
+                </div>
+                <div class="col">
+                    <article>
+                        <figure>
+                            <a href="phone_cases_img/huawei_p30_red.jpg">
+                                <img src="phone_cases_img/huawei_p30_red.jpg" alt="huaweip30"/>
+                            </a>
+                            <figcaption>HuaWei P30 silicon case red</figcaption>
+                        </figure>
+                    </article>
+                </div>
+                <div class="col">
+                    <article>
+                        <figure>
+                            <a href="phone_cases_img/samsung_note20_brown.jpg">
+                                <img src="phone_cases_img/samsung_note20_brown.jpg" alt="Samsungnote20"/>
+                            </a>
+                            <figcaption>Samsung note20 Leather cover - brown</figcaption>
+                        </figure>
+                    </article>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <article>
+                        <figure>
+                            <a href="phone_cases_img/iphone_12_starrysky.jpg">
+                                <img src="phone_cases_img/iphone_12_starrysky.jpg" alt="iPhone12"/>
+                            </a>
+                            <figcaption>iPhone 12 Starry Sky</figcaption>
+                        </figure>
+                    </article>
+                </div>
+                <div class="col">
+                    <article>
+                        <figure>
+                            <a href="phone_cases_img/iphone_x_dog.jpg">
+                                <img src="phone_cases_img/iphone_x_dog.jpg" alt="iPhoneX"/>
+                            </a>
+                            <figcaption>iPhone X Dog</figcaption>
+                        </figure>
+                    </article>
+                </div>
+                <div class="col">
+                    <article>
+                        <figure>
+                            <a href="phone_cases_img/oppo_doraemon.jpg">
+                                <img src="phone_cases_img/oppo_doraemon.jpg" alt="Oppo"/>
+                            </a>
+                            <figcaption>Oppo Doraemon</figcaption>
+                        </figure>
+                    </article>
+                </div>
+            </div>
+        </div>
     </main>        
 </body>
 
