1aed608f11c3c939dd523e6321877169ec73ba02
KahWei <kahweing96@gmail.com>
2020-11-19 02:55:37 +0800

    Use script tag to redirect after registration instead of header.

--- 1004_Project/page_incs/nav.inc.php
+++ 1004_Project/page_incs/nav.inc.php
@@ -1,5 +1,5 @@
-<nav class="navbar navbar-expand-sm navbar-light" id="navbar">
-    <a class="navbar-brand" href="../pages/home.php">
+<nav class="navbar navbar-expand-sm navbar-light text-right" id="navbar">
+    <a class="navbar-brand" href="index.php">
         <img src="//cdn.shopify.com/s/files/1/0254/0516/1520/files/logo_2048x.gif?v=1561039465" alt="in a nutshell – kurzgesagt" height="30" style="height:30px;">
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
@@ -8,67 +8,68 @@
     <div class="collapse navbar-collapse" id="collapsibleNavbar">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="../pages/home.php">Home</a>
+                <a class="nav-link" href="/pages/index.php">Home</a>
             </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Shop
-                </a>
+            <li class="nav-item">
+                <a class="nav-item dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Shop</a>                 
+                    <i class="material-icons" style="font-size:1em">store</i>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="/pages/products.php">All</a></li>
+                    <li><a class="dropdown-item" href="/filterpage//huawei.php">Huawei</a></li>
+                    <li><a class="dropdown-item" href="/filterpage//iphone.php">Iphone</a></li>
+                    <li><a class="dropdown-item" href="/filterpage//oppo.php">Oppo</a></li>
+                    <li><a class="dropdown-item" href="/filterpage//samsung.php">Samsung</a></li>
+                    </ul>
+            </li>
+            <div class="dropdown">
+                <a class="nav-item dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About
+                <span class="caret"></span></a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="../pages/products.php">All</a></li>
-                    <li><a class="dropdown-item" href="../filterpage/iphone.php">iPhone</a></li>
-                    <li><a class="dropdown-item" href="../filterpage/samsung.php">Samsung</a></li>
-                    <li><a class="dropdown-item" href="../filterpage/oppo.php">Oppo</a></li>
-                    <li><a class="dropdown-item" href="../filterpage/huawei.php">Huawei</a></li>
+                    <li><a class="dropdown-item" href="/pages/aboutus.php">About Us</a></li>
+                    <li><a class="dropdown-item" href="/pages/contactus.php">Contact</a></li>
                 </ul>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="../pages/aboutus.php">About Us</a></li>
-                    <li><a class="dropdown-item" href="../pages/contactus.php">Contact</a></li>
-                </ul>
-            </li>         
-        </ul>       
+            </div>
         </ul>
         <ul class="navbar-nav ml-auto">
-            <li class="link-icons">
-                <a class="nav-link" title="Shopping Cart" href="home.php?page=cart">
-                    <i class="fas fa-shopping-cart" style="font-size:1.8em"></i>
+            <li class="nav-item">
+                <a class="nav-link" title="Shopping Cart" href="#">
+                    <i class="material-icons" style="font-size:2em">shopping_cart</i>
                 </a>
             </li>
             <?php
-                if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) { //Means user is logged in display logout
-            ?>
+            if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) { //Means user is logged in display logout?>
                 <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="material-icons" style="font-size:2em">account_circle</i>
+                        <i class="material-icons" style="font-size:2em">account_circle</i>  
                     </a>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="../pages/account.php">Account</a>
+                        <a class="dropdown-item" href="/pages/account.php">Account</a>
                         <a class="dropdown-item" href="">Order History</a>
                     </div>
                 </li>
+           
+           
                 <li class="nav-item">
-                    <a class="nav-link" title="Log out" href="../pages/logout.php">
+                    <a class="nav-link" title="Log out" href="/pages/logout.php">
                         <i class="material-icons" style="font-size:2em">exit_to_app</i>
                     </a>
-                </li>
-                <?php
+                </li>         
+                <?php 
             } else //User is not logged in diplay login/register
-            {
+            { 
                 ?>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="material-icons" style="font-size:2em">account_circle</i>
+                        <i class="material-icons" style="font-size:2em">account_circle</i>  
                     </a>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="../pages/login.php">Log in</a>
-                        <a class="dropdown-item" href="../pages/register.php">Register</a>
+                        <a class="dropdown-item" href="/pages/login.php">Log in</a>
+                        <a class="dropdown-item" href="/pages/register.php">Register</a>
                     </div>
                 </li>
-                <?php
-                }
-                ?>
-          </ul>
-    </div>
+<?php 
+            } 
+            ?>
+        </ul>
+    </div>  
 </nav>
--- 1004_Project/processes/process_register.php
+++ 1004_Project/processes/process_register.php
@@ -1,45 +1,31 @@
 <?php
 session_start(); //start session
-?>
-   <?php         
 $fname = $lname = $email = $pwd_hashed = $errorMsg = "";
 $success = true;
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-    
     if (!empty($_POST["fname"])) {
         $fname = sanitize_input($_POST["fname"]);
     }
-    
-    
     if (empty($_POST["lname"])) {
         $errorMsg .= "Last name is required.<br>";
         $success = false;
     } else {
         $lname = sanitize_input($_POST["lname"]);
     }
-    
-  
     if (empty($_POST["email"])) {
         $errorMsg .= "Email is required.<br>";
         $success = false;
     } else {
         $email = sanitize_input($_POST["email"]);
-        
-      
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $errorMsg .= "Invalid email format.<br>";
             $success = false;
         }
     }
-    
-
     if (empty($_POST["pwd"]) || empty($_POST["pwd_confirm"])) {
         $errorMsg .= "Password and confirmation are required.<br>";
         $success = false;
     } else {
-     
         if ($_POST["pwd"] != $_POST["pwd_confirm"]) {
             $errorMsg .= "Passwords do not match.<br>";
             $success = false;
@@ -47,15 +33,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $pwd_hashed = password_hash($_POST["pwd"], PASSWORD_DEFAULT);
         }
     }
-     
 } else {
     echo "<h2>This page is not meant to be run directly.</h2>";
     echo "<p>You can register at the link below:</p>";
     echo "<a href='../pages/register.php'>Go to Sign up page...</a>";
     exit();
 }
-
-
 
 // Helper function that checks input for malicious or unwanted content.
 function sanitize_input($data) {
@@ -67,48 +50,35 @@ function sanitize_input($data) {
 
 function saveMemberToDB() {
     global $id, $fname, $lname, $email, $pwd_hashed, $errorMsg, $success;
-    
+
     // Create database connection.
     //$config = parse_ini_file('../../private/db-config.ini');
     //$conn = new mysqli($config['servername'], $config['username'],
-      //      $config['password'], $config['dbname']);
+    //      $config['password'], $config['dbname']);
 // Create connection
     $servername = "localhost";
     $username = "root";
     $password = "kahwei";
     $dbname = "1004_Project";
-
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-    
+    $conn = new mysqli($servername, $username, $password, $dbname);
     // Check connection
-    if ($conn->connect_error)
-    {
+    if ($conn->connect_error) {
         $errorMsg = "Connection failed: " . $conn->connect_error;
         $success = false;
     } else {
         // Prepare the statement:
         $stmt = $conn->prepare("INSERT INTO member (fname, lname, email, password) VALUES (?, ?, ?, ?)");
-       // $id = mysqli_insert_id($conn);
-        $_SESSION['username']=$fname; //get fname after registration 
+        // $id = mysqli_insert_id($conn);
+        $_SESSION['username'] = $fname; //get fname after registration 
         $_SESSION["loggedin"] = true;
-          
         // Bind & execute the query statement:
         $stmt->bind_param("ssss", $fname, $lname, $email, $pwd_hashed);
-        
-         $result = $stmt->execute();
-
+        $result = $stmt->execute();
         if ($result) {
             $id = $stmt->insert_id;
             $_SESSION['id'] = $id;
-        }
-   
- else {
-                
- 
-      // $_SESSION["id"]=$last_id;
-     //   if (!$stmt->execute()) {
+        } else {
             $errorMsg = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
             $success = false;
             $_SESSION["loggedin"] = false;
@@ -122,32 +92,32 @@ $conn = new mysqli($servername, $username, $password, $dbname);
     <head>
         <title>Registration Results</title>
         <?php
-           include "../page_incs/head.inc.php";
+        include "../page_incs/head.inc.php";
         ?>
     </head>
     <body>
         <?php
-            include "../page_incs/nav.inc.php";
-        ?>
-        
-        <main class="container">
-            
+        include "../page_incs/nav.inc.php";
+        ?>       
+        <main class="container">           
             <?php
             if ($success) {
                 saveMemberToDB();
-                header('Location: /pages/welcome.php');
+                ?>
+                <script type="text/javascript">
+                    window.location.href = "/pages/welcome.php";
+                </script>
+                <?php
             } else {
                 echo "<h3>Oops!</h3>";
                 echo "<h4>The following input errors were detected:</h4>";
                 echo "<p>" . $errorMsg . "</p>";
                 echo '<button class="btn btn-danger hBack">Return to Sign Up</button>';
             }
-            
-            ?>
-            
+            ?>            
         </main>
     </body>
-     <?php
-            include "../page_incs/footer.inc.php";
-        ?>
+    <?php
+    include "../page_incs/footer.inc.php";
+    ?>
 </html>
