0835daa1b65c0af2989809202da1e7ec6ef648aa
KahWei <kahweing96@gmail.com>
2020-11-20 01:33:43 +0800

    Home Page to load from DB

--- 1004_Project/pages/home.php
+++ 1004_Project/pages/home.php
@@ -10,11 +10,11 @@ session_start(); //start session
         article {
             text-align: center;
         }
-        
+
         #companyInfo {
             margin-top: -100px;
         }
-        
+
         #logo {
             height: 200px;
         }
@@ -33,105 +33,75 @@ session_start(); //start session
     </header>
     <main class="container">
         <h3>Popular items</h3>
-        <!--put image in 3 column -->
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <article>
-                        <figure>
-                            <a href="../images/phone_cases_img/iphone_11_clearcase.jpg">
-                                <img src="../images/phone_cases_img/iphone_11_clearcase.jpg" alt="iphone11"/>
-                            </a>
-                            <figcaption>iPhone 11 clear case</figcaption>
-                        </figure>
-                    </article>
-                </div>
-                <div class="col">
-                    <article >
-                        <figure>
-                            <a href="../images/phone_cases_img/iphone_12_kumquat.jpg">
-                                <img src="../images/phone_cases_img/iphone_12_kumquat.jpg" alt="iphone12"/>
-                            </a>
-                            <figcaption>iPhone 12 Silicon case - Kumquat</figcaption>
-                        </figure>
-                    </article>
-                </div>
-                <div class="col">
-                    <article >
-                        <figure>
-                            <a href="../images/phone_cases_img/iphone_12_Green.jpg">
-                                <img src="../images/phone_cases_img/iphone_12_Green.jpg" alt="iphone12"/>
-                            </a>
-                            <figcaption>iPhone 12 Silicon case - Green</figcaption>
-                        </figure>
-                    </article>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <article>
-                        <figure>
-                            <a href="../images/phone_cases_img/samsung_note20_clear.jpg">
-                                <img src="../images/phone_cases_img/samsung_note20_clear.jpg" alt="Samsungnote20"/>
-                            </a>
-                            <figcaption>Samsung note20 clear</figcaption>
-                        </figure>
-                    </article>
-                </div>
-                <div class="col">
-                    <article>
-                        <figure>
-                            <a href="../images/phone_cases_img/huawei_p30_red.jpg">
-                                <img src="../images/phone_cases_img/huawei_p30_red.jpg" alt="huaweip30"/>
-                            </a>
-                            <figcaption>HuaWei P30 silicon case red</figcaption>
-                        </figure>
-                    </article>
-                </div>
-                <div class="col">
-                    <article>
-                        <figure>
-                            <a href="../images/phone_cases_img/samsung_note20_brown.jpg">
-                                <img src="../images/phone_cases_img/samsung_note20_brown.jpg" alt="Samsungnote20"/>
-                            </a>
-                            <figcaption>Samsung note20 Leather cover - brown</figcaption>
-                        </figure>
-                    </article>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <article>
-                        <figure>
-                            <a href="../images/phone_cases_img/iphone_12_starrysky.jpg">
-                                <img src="../images/phone_cases_img/iphone_12_starrysky.jpg" alt="iPhone12"/>
-                            </a>
-                            <figcaption>iPhone 12 Starry Sky</figcaption>
-                        </figure>
-                    </article>
-                </div>
-                <div class="col">
-                    <article>
-                        <figure>
-                            <a href="../images/phone_cases_img/iphone_x_dog.jpg">
-                                <img src="../images/phone_cases_img/iphone_x_dog.jpg" alt="iPhoneX"/>
-                            </a>
-                            <figcaption>iPhone X Dog</figcaption>
-                        </figure>
-                    </article>
-                </div>
-                <div class="col">
-                    <article>
-                        <figure>
-                            <a href="../images/phone_cases_img/oppo_doraemon.jpg">
-                                <img src="../images/phone_cases_img/oppo_doraemon.jpg" alt="Oppo"/>
-                            </a>
-                            <figcaption>Oppo Doraemon</figcaption>
-                        </figure>
-                    </article>
-                </div>
-            </div>
+        <?php
+//Nicholas db connect
+//$con = mysqli_connect("localhost", "root", "E*z?%-iD8#hr", "1004_project");
+//Kah Wei db connect
+        $con = mysqli_connect("localhost", "root", "kahwei", "1004_project");
 
+
+//// The amounts of products to show on each page
+        $num_products_on_each_page = 6;
+
+// The current page, in the URL this will appear as index.php?page=products&p=1, index.php?page=products&p=2, etc...
+        $current_page = isset($_GET['p']) && is_numeric($_GET['p']) ? (int) $_GET['p'] : 1;
+
+// Select products ordered by the date added
+        $stmt = $con->prepare("SELECT * FROM products ORDER BY p_price ASC LIMIT ?,?");
+
+// bindValue will allow us to use integer in the SQL statement, we need to use for LIMIT
+        $var1 = (($current_page - 1) * $num_products_on_each_page);
+
+        $stmt->bind_param('ii', $var1, $num_products_on_each_page);
+        $stmt->execute();
+
+// Fetch the products from the database and return the result as an Array
+        $result = $stmt->get_result();
+        $products = $result->fetch_all(MYSQLI_ASSOC);
+
+// Get the total number of products
+        $total_products = $result->num_rows;
+        ?> 
+            <div class="row">
+                <div class="grid-container">
+
+                    <?php foreach ($products as $product): ?>
+
+                        <div class="grid-item">
+                            <article>
+                                <figure>
+                                    <a href="index.php?page=product&id=<?= $product['product_id'] ?>" class="product">
+                                        <img src="../images/phone_cases_img/<?= $product['p_img'] ?>" width="80px" height="150px" class="phone_image" alt="<?= $product['p_name'] ?>">
+                                    </a>
+
+                                    <h5 class="text-body"><?= $product['p_name'] ?></h5>
+                                    <h5 class="text-info">&dollar;<?= $product['p_price'] ?></h5>
+
+                                    <div class="form-group">
+                                        <label for="quantity"></label>
+                                        <input class="form-control" type="number" id="quantity" name="quantity"
+                                               placeholder="1">
+                                    </div>
+
+                                    <input type="hidden" name="hidden_name" value="<?= $product['p_name'] ?>">
+                                    <input type="hidden" name="hidden_price" value="<?= $product['p_price'] ?>">
+                                    <input type="submit" name="add" style="margin-top: 5px;" class="btn btn-success" value="Add to Cart">
+                                </figure>
+                            </article>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        </div>
+
+        <div class="buttons">
+            <?php if ($current_page > 1): ?>
+                <a href="index.php?page=products&p=<?= $current_page - 1 ?>">Prev</a>
+            <?php endif; ?>
+
+            <?php if ($total_products > ($current_page * $num_products_on_each_page) - $num_products_on_each_page + count($products)): ?>
+                <a href="index.php?page=products&p=<?= $current_page + 1 ?>">Next</a>
+            <?php endif; ?>
         </div>
     </main>        
 </body>
