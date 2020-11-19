fceac2d6815ca63cb233b4e71cbe5d600a206a2b
juanjuan7699 <71871190+juanjuan7699@users.noreply.github.com>
2020-11-19 18:15:01 +0800

    changed navbar logo

--- 1004_Project/page_incs/nav.inc.php
+++ 1004_Project/page_incs/nav.inc.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-sm navbar-light text-right" id="navbar">
     <a class="navbar-brand" href="/pages/home.php">
-        <img src="//cdn.shopify.com/s/files/1/0254/0516/1520/files/logo_2048x.gif?v=1561039465" alt="in a nutshell – kurzgesagt" height="30" style="height:30px;">
+        <img src="../images/phonix_logo_black.png" alt="phonix" height="40">
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
         <span class="navbar-toggler-icon"></span>
