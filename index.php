c243f9194f68f9a511e486d9e2d7c3df6a1e3f15
guancong <guancong96@gmail.com>
2020-11-20 22:41:23 +0800

    UI overhaul

--- index.php
+++ index.php
@@ -85,7 +85,7 @@ session_start(); //start session
                                 <div class="tab-single">
                                     <div class="row">
                                         <?php foreach ($products as $product): ?>
-                                            <div class="col-xl-3 col-lg-4 col-md-4 col-12">
+                                            <div class="col-xl-3 col-lg-4 col-md-3 col-12">
                                                 <div class="single-product">
                                                     <div class="product-img">
                                                         <a href="product-details.html">
@@ -163,78 +163,6 @@ session_start(); //start session
     </div>
     <!-- End -->
 
-    <!-- Start Shop Home  -->
-    <section class="shop-home-list section">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4 col-md-6 col-12">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="shop-section-title">
-                                <h1>On sale</h1>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Start Single List  -->
-                    <div class="single-list">
-                        <div class="row">
-                            <div class="col-lg-6 col-md-6 col-12">
-                                <div class="list-image overlay">
-                                    <img src="https://via.placeholder.com/115x140" alt="#">
-                                    <a href="#" class="buy"><i class="fa fa-shopping-bag"></i></a>
-                                </div>
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-12 no-padding">
-                                <div class="content">
-                                    <h4 class="title"><a href="#">Licity jelly leg flat Sandals</a></h4>
-                                    <p class="price with-discount">$59</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Single List  -->
-                    <!-- Start Single List  -->
-                    <div class="single-list">
-                        <div class="row">
-                            <div class="col-lg-6 col-md-6 col-12">
-                                <div class="list-image overlay">
-                                    <img src="https://via.placeholder.com/115x140" alt="#">
-                                    <a href="#" class="buy"><i class="fa fa-shopping-bag"></i></a>
-                                </div>
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-12 no-padding">
-                                <div class="content">
-                                    <h5 class="title"><a href="#">Licity jelly leg flat Sandals</a></h5>
-                                    <p class="price with-discount">$44</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Single List  -->
-                    <!-- Start Single List  -->
-                    <div class="single-list">
-                        <div class="row">
-                            <div class="col-lg-6 col-md-6 col-12">
-                                <div class="list-image overlay">
-                                    <img src="https://via.placeholder.com/115x140" alt="#">
-                                    <a href="#" class="buy"><i class="fa fa-shopping-bag"></i></a>
-                                </div>
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-12 no-padding">
-                                <div class="content">
-                                    <h5 class="title"><a href="#">Licity jelly leg flat Sandals</a></h5>
-                                    <p class="price with-discount">$89</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Single List  -->
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- End Shop Home  -->
-
     <!-- Start Cowndown Area -->
     <section class="cown-down">
         <div class="section-inner ">
@@ -242,16 +170,16 @@ session_start(); //start session
                 <div class="row">
                     <div class="col-lg-6 col-12 padding-right">
                         <div class="image">
-                            <img src="https://via.placeholder.com/750x590" alt="#">
+                            <img src="images/deal_of_day.jpg" alt="#">
                         </div>
                     </div>
                     <div class="col-lg-6 col-12 padding-left">
-                        <div class="content">
+                        <div class="content align-content-center">
                             <div class="heading-block">
                                 <p class="small-title">Deal of day</p>
                                 <h3 class="title">Beatutyful dress for women</h3>
                                 <p class="text">Suspendisse massa leo, vestibulum cursus nulla sit amet, frungilla placerat lorem. Cars fermentum, sapien. </p>
-                                <h1 class="price">$1200 <s>$1890</s></h1>
+                                <h1 class="price">$25 <s>$50</s></h1>
                                 <div class="coming-time">
                                     <div class="clearfix" data-countdown="2021/02/30"></div>
                                 </div>
