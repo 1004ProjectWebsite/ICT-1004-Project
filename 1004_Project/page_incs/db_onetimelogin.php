269dee6cd515aace4590c8027863fa243311be38
guancong <guancong96@gmail.com>
2020-11-26 17:58:57 +0800

    Updates: 1. Fixed the footer for register and login

--- 1004_Project/page_incs/db_onetimelogin.php
+++ 1004_Project/page_incs/db_onetimelogin.php
@@ -3,10 +3,10 @@
 //connection details for database
 
 //  KahWei DB connect
-   $servername = "localhost";
-   $username = "root";
-   $password = "kahwei";
-  $dbname = "1004_Project";
+//   $servername = "localhost";
+//   $username = "root";
+//   $password = "kahwei";
+//  $dbname = "1004_Project";
 
 //  Nicholas DB connect
 //   $servername = "localhost";
@@ -15,10 +15,10 @@
 //  $dbname = "1004_Project";
 
 //GC DB connect
-//$servername = "localhost";
-//$username = "root";
-//$password = "password";
-//$dbname = "1004_project";
+$servername = "localhost";
+$username = "root";
+$password = "password";
+$dbname = "1004_project";
 
 // Login for database
 
@@ -26,11 +26,11 @@
 //$con = mysqli_connect("localhost", "root", "E*z?%-iD8#hr", "1004_project");
 
 //Kah Wei db connect
-$con = mysqli_connect("localhost", "root", "kahwei", "1004_project");
+//$con = mysqli_connect("localhost", "root", "kahwei", "1004_project");
 
 //sj db
 //$con = mysqli_connect("localhost", "root", "SJTey99607", "1004_proj");
 
 //GC db
-//$con = mysqli_connect("localhost", "root", "password", "1004_project");
+$con = mysqli_connect("localhost", "root", "password", "1004_project");
 ?>
--- 1004_Project/pages/register.php
+++ 1004_Project/pages/register.php
@@ -8,61 +8,66 @@
             include "../page_incs/nav.inc.php";
         ?>
 
-        <div class="layout-form">
-            <form action="process_register.php" method="post">
-                <h2>Sign Up</h2>
-                <p>Please fill in this form to create an account!</p>
-                <hr>
-                <div class="form-group">
-                    <div class="input-group">
-                        <span class="input-group-addon"><i class="fa fa-user"></i></span>
-                        <input class="form-control" type="text" id="fname"
-                               maxlength="45" name="fname" placeholder="Enter first name">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="input-group">
-                        <span class="input-group-addon"><i class="fa fa-user"></i></span>
-                        <input class="form-control" type="text" id="lname"
-                               required maxlength="45" name="lname"
-                               placeholder="Enter last name">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="input-group">
-                        <span class="input-group-addon"><i class="fa fa-paper-plane"></i></span>
-                        <input class ="form-control" type="email" id="email" required name="email"
-                               placeholder="Enter email">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="input-group">
-                        <span class="input-group-addon"><i class="fa fa-lock"></i></span>
-                        <input class="form-control" type="password" id="pwd" required name="pwd"
-                               placeholder="Enter password" minlength="6">
 
-                    </div>
-                    <p class="help-block"> Password should be at least 6 characters</p>
-                </div>
-                <div class="form-group">
-                    <div class="input-group">
+        <main role="main" class="row flex-grow-1 w-100 align-items-center">
+            <div class="mx-auto">
+                <div class="layout-form">
+                    <form action="process_register.php" method="post">
+                        <h2>Sign Up</h2>
+                        <p>Please fill in this form to create an account!</p>
+                        <hr>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                                <input class="form-control" type="text" id="fname"
+                                       maxlength="45" name="fname" placeholder="Enter first name">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                                <input class="form-control" type="text" id="lname"
+                                       required maxlength="45" name="lname"
+                                       placeholder="Enter last name">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-paper-plane"></i></span>
+                                <input class ="form-control" type="email" id="email" required name="email"
+                                       placeholder="Enter email">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                                <input class="form-control" type="password" id="pwd" required name="pwd"
+                                       placeholder="Enter password" minlength="6">
+
+                            </div>
+                            <p class="help-block"> Password should be at least 6 characters</p>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-group">
                         <span class="input-group-addon">
                             <i class="fa fa-lock"></i>
                             <i class="fa fa-check"></i>
                         </span>
-                        <input class="form-control" type="password" id="pwd_confirm" required name="pwd_confirm"     
-                               placeholder="Confirm password">
-                    </div>
+                                <input class="form-control" type="password" id="pwd_confirm" required name="pwd_confirm"
+                                       placeholder="Confirm password">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="checkbox-inline"><input type="checkbox" required="required"> I accept the <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a></label>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary btn-lg">Sign Up</button>
+                        </div>
+                    </form>
+                    <div class="text-center my-2">Already have an account? <a href="index.php?page=login" style="color:black">Login here</a></div>
                 </div>
-                <div class="form-group">
-                    <label class="checkbox-inline"><input type="checkbox" required="required"> I accept the <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a></label>
-                </div>
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary btn-lg">Sign Up</button>
-                </div>
-            </form>
-            <div class="text-center">Already have an account? <a href="index.php?page=login" style="color:black">Login here</a></div>
-        </div>
+            </div>
+        </main>
     </body>
 
 <?php
