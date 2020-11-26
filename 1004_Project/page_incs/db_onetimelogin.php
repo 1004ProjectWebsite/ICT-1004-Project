b8315df5a0d6fea40ec9a6dadcb3d75fdf3cdaaf
juanjuan7699 <71871190+juanjuan7699@users.noreply.github.com>
2020-11-26 20:18:17 +0800

    add session start to checkout

--- 1004_Project/page_incs/db_onetimelogin.php
+++ 1004_Project/page_incs/db_onetimelogin.php
@@ -32,10 +32,10 @@ $dbname = "1004_proj";
 //$con = mysqli_connect("localhost", "root", "E*z?%-iD8#hr", "1004_project");
 
 //Kah Wei db connect
-$con = mysqli_connect("localhost", "root", "kahwei", "1004_project");
+//$con = mysqli_connect("localhost", "root", "kahwei", "1004_project");
 
 //sj db
-//$con = mysqli_connect("localhost", "root", "SJTey99607", "1004_proj");
+$con = mysqli_connect("localhost", "root", "SJTey99607", "1004_proj");
 
 //GC db
 //$con = mysqli_connect("localhost", "root", "password", "1004_project");
